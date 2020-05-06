d6f4159800bb7c8f4827387da1268e1fc47443ca
Jessica Weiss <Jessica.Weiss@stud.bs-aarau.ch>
2020-05-06 20:07:29 +0200

    Projektdateien hinzufügen.

---
+++ Form1.Designer.cs
@@ -0,0 +1,153 @@
+﻿namespace Weiss_Jessica_Phytagoras
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Erforderliche Designervariable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Verwendete Ressourcen bereinigen.
+        /// </summary>
+        /// <param name="disposing">True, wenn verwaltete Ressourcen gelöscht werden sollen; andernfalls False.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Vom Windows Form-Designer generierter Code
+
+        /// <summary>
+        /// Erforderliche Methode für die Designerunterstützung.
+        /// Der Inhalt der Methode darf nicht mit dem Code-Editor geändert werden.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblA = new System.Windows.Forms.Label();
+            this.txtBoxA = new System.Windows.Forms.TextBox();
+            this.lblB = new System.Windows.Forms.Label();
+            this.txtBoxB = new System.Windows.Forms.TextBox();
+            this.lblC = new System.Windows.Forms.Label();
+            this.txtBoxC = new System.Windows.Forms.TextBox();
+            this.btnCBerechnen = new System.Windows.Forms.Button();
+            this.lblEingabekontrolle = new System.Windows.Forms.Label();
+            this.btnABBerechnen = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // lblA
+            // 
+            this.lblA.AutoSize = true;
+            this.lblA.Location = new System.Drawing.Point(88, 98);
+            this.lblA.Name = "lblA";
+            this.lblA.Size = new System.Drawing.Size(28, 20);
+            this.lblA.TabIndex = 0;
+            this.lblA.Text = "A: ";
+            // 
+            // txtBoxA
+            // 
+            this.txtBoxA.Location = new System.Drawing.Point(135, 98);
+            this.txtBoxA.Name = "txtBoxA";
+            this.txtBoxA.Size = new System.Drawing.Size(100, 26);
+            this.txtBoxA.TabIndex = 1;
+            // 
+            // lblB
+            // 
+            this.lblB.AutoSize = true;
+            this.lblB.Location = new System.Drawing.Point(88, 151);
+            this.lblB.Name = "lblB";
+            this.lblB.Size = new System.Drawing.Size(24, 20);
+            this.lblB.TabIndex = 2;
+            this.lblB.Text = "B:";
+            // 
+            // txtBoxB
+            // 
+            this.txtBoxB.Location = new System.Drawing.Point(135, 151);
+            this.txtBoxB.Name = "txtBoxB";
+            this.txtBoxB.Size = new System.Drawing.Size(100, 26);
+            this.txtBoxB.TabIndex = 3;
+            // 
+            // lblC
+            // 
+            this.lblC.AutoSize = true;
+            this.lblC.Location = new System.Drawing.Point(88, 203);
+            this.lblC.Name = "lblC";
+            this.lblC.Size = new System.Drawing.Size(28, 20);
+            this.lblC.TabIndex = 5;
+            this.lblC.Text = "C: ";
+            // 
+            // txtBoxC
+            // 
+            this.txtBoxC.Location = new System.Drawing.Point(135, 197);
+            this.txtBoxC.Name = "txtBoxC";
+            this.txtBoxC.Size = new System.Drawing.Size(100, 26);
+            this.txtBoxC.TabIndex = 6;
+            // 
+            // btnCBerechnen
+            // 
+            this.btnCBerechnen.Location = new System.Drawing.Point(319, 168);
+            this.btnCBerechnen.Name = "btnCBerechnen";
+            this.btnCBerechnen.Size = new System.Drawing.Size(131, 41);
+            this.btnCBerechnen.TabIndex = 7;
+            this.btnCBerechnen.Text = "C Berechnen";
+            this.btnCBerechnen.UseVisualStyleBackColor = true;
+            this.btnCBerechnen.Click += new System.EventHandler(this.btnCBerechnen_Click);
+            // 
+            // lblEingabekontrolle
+            // 
+            this.lblEingabekontrolle.AutoSize = true;
+            this.lblEingabekontrolle.Location = new System.Drawing.Point(135, 287);
+            this.lblEingabekontrolle.Name = "lblEingabekontrolle";
+            this.lblEingabekontrolle.Size = new System.Drawing.Size(0, 20);
+            this.lblEingabekontrolle.TabIndex = 8;
+            // 
+            // btnABBerechnen
+            // 
+            this.btnABBerechnen.Location = new System.Drawing.Point(306, 100);
+            this.btnABBerechnen.Name = "btnABBerechnen";
+            this.btnABBerechnen.Size = new System.Drawing.Size(156, 38);
+            this.btnABBerechnen.TabIndex = 9;
+            this.btnABBerechnen.Text = "A / B Berechnen";
+            this.btnABBerechnen.UseVisualStyleBackColor = true;
+            this.btnABBerechnen.Click += new System.EventHandler(this.btnABBerechnen_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.btnABBerechnen);
+            this.Controls.Add(this.lblEingabekontrolle);
+            this.Controls.Add(this.btnCBerechnen);
+            this.Controls.Add(this.txtBoxC);
+            this.Controls.Add(this.lblC);
+            this.Controls.Add(this.txtBoxB);
+            this.Controls.Add(this.lblB);
+            this.Controls.Add(this.txtBoxA);
+            this.Controls.Add(this.lblA);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblA;
+        private System.Windows.Forms.TextBox txtBoxA;
+        private System.Windows.Forms.Label lblB;
+        private System.Windows.Forms.TextBox txtBoxB;
+        private System.Windows.Forms.Label lblC;
+        private System.Windows.Forms.TextBox txtBoxC;
+        private System.Windows.Forms.Button btnCBerechnen;
+        private System.Windows.Forms.Label lblEingabekontrolle;
+        private System.Windows.Forms.Button btnABBerechnen;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,133 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Weiss_Jessica_Phytagoras
+{
+
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnCBerechnen_Click(object sender, EventArgs e)
+
+        {
+            //0.Defaultwerte setze
+            txtBoxA.BackColor = Color.White;
+            txtBoxB.BackColor = Color.White;
+            txtBoxC.BackColor = Color.White;
+
+            //1.Variablen definieren
+            double a = 0;
+            double b = 0;
+            double c = 0;
+
+            //2. Eingabekontrolle
+            if (double.TryParse(txtBoxA.Text, out a) == false)
+            {
+                lblEingabekontrolle.Text = "Bitte geben Sie eine korrekte Zahl ein.";
+                txtBoxA.BackColor = Color.Red;
+                txtBoxA.Text = "";
+                txtBoxA.Focus();
+                return;
+            }
+            if (double.TryParse(txtBoxB.Text, out b) == false)
+            {
+                lblEingabekontrolle.Text = "Bitte geben Sie eine korrekte Zahl ein.";
+                txtBoxB.BackColor = Color.Red;
+                txtBoxB.Text = "";
+                txtBoxB.Focus();
+                return;
+            }
+
+            //3.Rechnen
+
+            //Hypothenuse berechnen
+            c = Math.Sqrt( Math.Pow(a, 2) + Math.Pow(b, 2));
+
+            //Ausgabe
+            txtBoxC.Text = c.ToString();
+            lblEingabekontrolle.Text = "Die Hypothenuse hat eine Länge von " + c.ToString();
+
+         }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnABBerechnen_Click(object sender, EventArgs e)
+        {
+            //0.Defaultwerte setze
+            txtBoxA.BackColor = Color.White;
+            txtBoxB.BackColor = Color.White;
+            txtBoxC.BackColor = Color.White;
+
+            //1.Variablen definieren
+            double a = 0;
+            double b = 0;
+            double c = 0;
+
+            //Abgefüllte Felder kontrollieren
+            if (double.TryParse(txtBoxC.Text, out c) == false)
+            {
+                lblEingabekontrolle.Text = "Bitte geben Sie eine korrekte Zahl ein.";
+                txtBoxC.BackColor = Color.Red;
+                txtBoxC.Text = "";
+                txtBoxC.Focus();
+                return;
+            }
+
+            if (txtBoxA.Text == "" && txtBoxB.Text != "")
+            {
+                if (double.TryParse(txtBoxB.Text, out b) == false)
+                {
+                    lblEingabekontrolle.Text = "Bitte geben Sie eine korrekte Zahl ein.";
+                    txtBoxB.BackColor = Color.Red;
+                    txtBoxB.Text = "";
+                    txtBoxB.Focus();
+                    return;
+                }
+
+                a = Math.Sqrt(Math.Pow(c, 2) - Math.Pow(b, 2));
+                lblEingabekontrolle.Text = "A hat eine Länge von " + a.ToString();
+                txtBoxA.Text = a.ToString();
+
+            } else if(txtBoxA.Text != "" && txtBoxB.Text == "")
+            {
+                if (double.TryParse(txtBoxA.Text, out a) == false)
+                {
+                    lblEingabekontrolle.Text = "Bitte geben Sie eine korrekte Zahl ein.";
+                    txtBoxA.BackColor = Color.Red;
+                    txtBoxA.Text = "";
+                    txtBoxA.Focus();
+                    return;
+                }
+
+                b = Math.Sqrt(Math.Pow(c, 2) - Math.Pow(a, 2));
+                lblEingabekontrolle.Text = "B hat eine Länge von " + b.ToString();
+                txtBoxB.Text = b.ToString();
+
+            }
+            else
+            {
+                lblEingabekontrolle.Text = "Bitte geben Sie eine korrekte Zahl ein.";
+                txtBoxA.BackColor = Color.Red;
+                txtBoxB.BackColor = Color.Red;
+                txtBoxA.Text = "";
+                txtBoxB.Text = "";
+                txtBoxA.Focus();
+                return;
+            }
+        }
+    }
+}
